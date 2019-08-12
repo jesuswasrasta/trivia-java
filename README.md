fd2ea7a54ab7694a5d7ef41f6914754ce9d9f59d
Ferdinando Santacroce <santacroce@intre.it>
2019-08-12 12:13:34 +0200

    First commit
    * update Maven and JUnit
    * add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trivia
+This is a shameless copy of [Ugly Trivia](https://github.com/jbrains/trivia).  
+I extracted here the original Java implementation, updating Maven dependecies and JUnit.  
---
+++ src/test/java/com/jesuswasrasta/trivia/SetupShould.java
@@ -0,0 +1,13 @@
+package com.jesuswasrasta.trivia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetupShould {
+
+    @Test
+    public void beFine() throws Exception {
+        assertTrue(true);
+    }
+}
