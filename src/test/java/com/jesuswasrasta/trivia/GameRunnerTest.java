851a489660a6c252b52a77e55b877707905eaebf
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-18 14:10:51 +0200

    BLUE - Small refactor to remove warnings

--- src/test/java/com/jesuswasrasta/trivia/GameRunnerTest.java
+++ src/test/java/com/jesuswasrasta/trivia/GameRunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameRunnerTest {
-    ByteArrayOutputStream stream;
+    private ByteArrayOutputStream stream;
 
     @BeforeEach
     void beforeEach(){
--- src/test/java/com/jesuswasrasta/trivia/GameTest.java
+++ src/test/java/com/jesuswasrasta/trivia/GameTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
-    ByteArrayOutputStream stream;
+    private ByteArrayOutputStream stream;
 
     @BeforeEach
     void beforeEach(){
