811b76c06899509ad97029dfd612377e8b1a2c83
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-18 14:40:46 +0200

    GREEN - Test against Golden master generated files

--- src/test/java/com/jesuswasrasta/trivia/GoldenMasterTest.java
+++ src/test/java/com/jesuswasrasta/trivia/GoldenMasterTest.java
@@ -14,4 +14,16 @@ public class GoldenMasterTest {
         GoldenMaster goldenMaster = new GoldenMaster();
         goldenMaster.generateFiles();
     }
+
+    @Test
+    void checkAgainstGoldenMaster() throws IOException {
+        GoldenMaster goldenMaster = new GoldenMaster();
+
+        for (long seed = 0; seed < GoldenMaster.MAX_SEED; seed++) {
+            String expected = goldenMaster.getGoldenMasterBySeed(seed);
+            String actual = goldenMaster.getGameResult(seed);
+
+            assertEquals(expected, actual);
+        }
+    }
 }
