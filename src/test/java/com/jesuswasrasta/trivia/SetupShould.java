f6a715ee6b93d1f81132b5e7f9b02d73be5a8d32
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-18 12:05:02 +0200

    BLUE - Refactor

--- src/test/java/com/jesuswasrasta/trivia/SetupShould.java
+++ src/test/java/com/jesuswasrasta/trivia/SetupShould.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SetupShould {
+class SetupShould {
 
     @Test
-    public void whenAGameIsCreatedNothingHappens() throws Exception {
+    void whenAGameIsCreatedNothingHappens() {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(stream);
         System.setOut(printStream);
