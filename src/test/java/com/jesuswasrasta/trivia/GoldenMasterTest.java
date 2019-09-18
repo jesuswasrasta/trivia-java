51ea2dfcbb842b461cc055639758c8fbf2d0e6e2
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-18 14:40:08 +0200

    Small refactor, file generation test disabled

--- src/test/java/com/jesuswasrasta/trivia/GoldenMasterTest.java
+++ src/test/java/com/jesuswasrasta/trivia/GoldenMasterTest.java
@@ -2,10 +2,14 @@ package com.jesuswasrasta.trivia;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoldenMasterTest {
     @Test
+    @Disabled("Use this only to (re)generate the Golden Master files")
     void generateFiles() throws IOException {
         GoldenMaster goldenMaster = new GoldenMaster();
         goldenMaster.generateFiles();
