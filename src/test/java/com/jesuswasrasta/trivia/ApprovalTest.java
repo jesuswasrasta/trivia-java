30f3fb5c21d198ba85b371fa388a87edba0755d6
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-20 18:00:14 +0200

    Using DiffReporter

--- src/test/java/com/jesuswasrasta/trivia/ApprovalTest.java
+++ src/test/java/com/jesuswasrasta/trivia/ApprovalTest.java
@@ -8,6 +8,7 @@ import com.jesuswasrasta.trivia.runner.GameRunner;
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.QuietReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,6 +31,32 @@ public class ApprovalTest {
         gameRunner.play(random);
         Approvals.verify(stream.toString());
     }
+
+    @Test
+    @UseReporter(DiffReporter.class)
+    public void gameTestWithDiffReporter() {
+        // Directions:
+        // You will need to have a diff reporter installed on you system to do this.
+        // 1) Run the test
+        // 2) Right click the result in your diff tool and select use whole file.
+        /*
+         * Currently supported Diff Tools are
+         * Mac:
+         * DiffMerge
+         * TkDiff
+         * Kaleidoscope
+         *
+         * Windows:
+         * BeyondCompare
+         * TortoiseDiff
+         * WinMerge
+         */
+        Random random = new Random(0);
+        GameRunner gameRunner = new GameRunner();
+        gameRunner.play(random);
+        Approvals.verify(stream.toString());
+    }
+
 
 
     private ByteArrayOutputStream getOutputStream() {
