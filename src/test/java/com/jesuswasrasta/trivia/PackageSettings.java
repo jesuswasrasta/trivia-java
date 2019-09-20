ba05226c7dff2d8f2cc9672253ffab62c7203d71
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-09-20 17:39:23 +0200

    Setting ApprovalTests subfolders for output files

---
+++ src/test/java/com/jesuswasrasta/trivia/PackageSettings.java
@@ -0,0 +1,16 @@
+package com.jesuswasrasta.trivia;
+
+/**
+ * If you want to setup ApprovalTests library, create this class
+ *
+ * @link https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/Configuration.md
+ */
+public class PackageSettings
+{
+  /**
+   * Setting a different base folders for output files
+   *
+   * @link https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/Configuration.md#alternative-base-directory-for-output-files
+   */
+  public static String UseApprovalSubdirectory = "approvals";
+}
